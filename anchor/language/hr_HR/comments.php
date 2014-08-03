d80c7afb28ec943a1bb83fcd74f820e628085fad
Mario Borna Mjertan <bor.mbm@gmail.com>
2014-08-03 23:53:09 +0200

    Create comments.php

---
+++ anchor/language/hr_HR/comments.php
@@ -0,0 +1,35 @@
+<?php
+
+return array(
+
+	'comments' => 'Komentari',
+	'nocomments_desc' => 'Još nema komentara.',
+	'editing_comment' => 'Uređivanje komentara',
+	'view_comment' => 'Pogledaj komentar',
+
+	// form fields
+	'name' => 'Ime',
+	'name_explain' => 'Ime autora',
+	'name_missing' => 'Molim unesi ime',
+
+	'email' => 'Email adresa',
+	'email_explain' => 'Autorov email',
+	'email_missing' => 'Molim unesi valjanu email adresu', // frontend message (appears on your site!)
+
+	'text' => 'Komentar',
+	'text_explain' => '',
+	'text_missing' => 'Molim unesi tekst komentara', // frontend message (appears on your site!)
+
+	'status' => 'Status',
+	'status_explain' => '',
+
+	// messages
+	'created' => 'Tvoj komentar je dodan', // frontend message (appears on your site!)
+	'updated' => 'Tvoj komentar je ažuriran',
+	'deleted' => 'Tvoj komentar je obrisan',
+
+	// email notification
+	'notify_subject' => 'Novi komentar je dodan',
+	'nofity_heading' => 'Novi komentar je poslan na tvoju stranicu'
+
+);
