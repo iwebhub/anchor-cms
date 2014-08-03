e6c7d942ff0071050002b5b922d42863a48781c7
Mario Borna Mjertan <bor.mbm@gmail.com>
2014-08-03 23:44:01 +0200

    Create categories.php

---
+++ anchor/language/hr_HR/categories.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+	'category' => 'Kategorija',
+	'categories' => 'Kategorije',
+
+	'create_category' => 'Napravi novu kategoriju',
+	'edit_category' => 'Uređivanje &ldquo;%s&rdquo;',
+
+	// form fields
+	'title' => 'Naslov',
+	'title_explain' => 'Naslov tvoje kategorije.',
+	'title_missing' => 'Molim unesi naslov',
+
+	'slug' => 'Slug',
+	'slug_explain' => 'Slug za tvoju kategoriju.',
+
+	'description' => 'Opis',
+	'description_explain' => 'O čemu je tvoja kategorija.',
+
+	// messages
+	'created' => 'Tvoja kategorija je dodana.',
+	'updated' => 'Tvoja kategorija je ažurirana.',
+	'deleted' => 'Tvoja kategorija je obrisana..',
+	'delete_error' => 'Moraš imati bar jednu kategoriju.',
+
+);
